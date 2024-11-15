9aa07632b4cd2290845e8212fe6f10d2dc59c2c9
Prosnis <gtfooooooo@gmail.com>
2024-11-15 21:00:31 +0700

    wip(WiUserStore: experimenting with Pinia integration

--- src/stores/WiUserStore.js
+++ src/stores/WiUserStore.js
@@ -1,4 +1,5 @@
-import { getAuth } from 'firebase/auth'
+import { getUserData } from '@/services/GetUserData'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
@@ -6,6 +7,22 @@ const auth = getAuth()
 
 export const useUserStore = defineStore('user', () => {
   const userUID = ref(null)
-  userUID.value = auth.currentUser.uid
-  return { userUID }
+  const user = ref(null)
+
+  const  getData = async () => {
+    const {user: userData} = await getUserData(userUID.value)
+    user.value = userData
+  }
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if(currentUser) {
+      userUID.value = currentUser.uid
+      getData()
+    }else {
+      userUID.value = null
+      user.value = null
+    }
+  })
+
+  return { userUID, user }
 })
